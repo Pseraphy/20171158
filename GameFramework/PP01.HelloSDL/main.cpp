3e9f1443e0d172250782a7540b7179c6d1318233
zjaaos0509 <zjaaos0509@gmail.com>
2018-11-22 16:10:51 +0900

    PP03.GameClass

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -1,53 +1,25 @@
-#include<SDL.h>
+#include "Game.h"
 
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
-void render();
-void update() {};
-void handleEvents() {};
-void clean() {};
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
+Game *g_game = 0;
+
+
 
 int main(int argc, char* argv[])
 {
-	if (init("Chapter02. BreakHello", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
-		bool g_bRunning = true;
-	}
-	else { return 1; }
-	while (g_bRunning) {
-		render();
-	}
-	clean();
+	g_game = new Game();
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
 
-	SDL_Quit();
 
-	return 0;
-}
-
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	while (g_game->running())
 	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
 	}
-	else {
-		return false;
-	}
-	return true;
-}
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
-	SDL_RenderClear(g_pRenderer);
+	g_game->clean();
+	return 0;
 
-	SDL_RenderPresent(g_pRenderer);
 }
